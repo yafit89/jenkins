88e7f6c684b5e3e2d97094a36a6ed65b970d8f61
yafit89 <yafitt364.com>
2024-12-18 16:18:01 +0200

    add files

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "davshe-technion-cicd-13578"
+    bucket         = "yafit-technion-cicd-13578"
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
